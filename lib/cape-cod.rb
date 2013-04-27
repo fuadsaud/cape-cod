b4e5223acd641f90f48bbd6160de2a66c364a907
Fuad Saud <fuadksd@gmail.com>
2013-04-27 19:21:00 -0300

    Refacorings on the public API

--- lib/cape-cod.rb
+++ lib/cape-cod.rb
@@ -7,73 +7,136 @@ module CapeCod
 
   require 'cape-cod/version'
 
-  ESCAPE_CODES = {
-    reset:          0,
-    bold:           1,
-    dark:           2,
-    italic:         3,
-    underline:      4,
-    blink:          5,
-    rapid_blink:    6,
-    negative:       7,
-    concealed:      8,
-    strikethrough:  9,
+  EFFECTS = {
+    reset:         0,
+    bold:          1,
+    dark:          2,
+    italic:        3,
+    underline:     4,
+    blink:         5,
+    rapid_blink:   6,
+    negative:      7,
+    concealed:     8,
+    strikethrough: 9
+  }.freeze
 
-    # Foreground color
-    black:         30,
-    red:           31,
-    green:         32,
-    yellow:        33,
-    blue:          34,
-    magenta:       35,
-    cyan:          36,
-    white:         37,
-
-    # Background color
-    on_black:      40,
-    on_red:        41,
-    on_green:      42,
-    on_yellow:     43,
-    on_blue:       44,
-    on_magenta:    45,
-    on_cyan:       46,
-    on_white:      47,
+  COLORS = {
+    black:    0,
+    red:      1,
+    green:    2,
+    yellow:   3,
+    blue:     4,
+    magenta:  5,
+    cyan:     6,
+    white:    7,
   }.freeze
 
   #
-  # Define helper methods for applying the escape codes
+  # Define helper methods for applying the escape codes.
   #
-  ESCAPE_CODES.each do |code, _|
-    define_method code do
-      CapeCod.apply_escape_sequence(code, self)
+  COLORS.each do |color, _|
+
+    #
+    # Instance methods for background and foreground colors.
+    #
+    define_method color do
+      CapeCod.foreground(color, self)
     end
 
-    define_singleton_method code do |obj = ''|
+    define_method "on_#{color}".to_s do
+      CapeCod.background(color, self)
+    end
+
+    #
+    # Singleton methods for background and foreground colors.
+    #
+    define_singleton_method color do |obj = ''|
       string = obj.to_s
-      return CapeCod.escape_sequence_for(code) if string.empty?
+      code = color_code_for(color, :foreground)
 
-      CapeCod.apply_escape_sequence(code, string)
+      return escape_sequence_for(code) if string.empty?
+
+      foreground(color, string)
+    end
+
+    define_singleton_method "on_#{color}" do |obj = ''|
+      string = obj.to_s
+      code = color_code_for(color, :background)
+
+      return escape_sequence_for(code) if string.empty?
+
+      background(color, string)
     end
   end
 
-  private
+  EFFECTS.each do |effect, _|
 
-  #
-  # Returns the ANSI escape sequence for a given escape +code+.
-  #
-  def self.escape_sequence_for(code)
-    "\e[#{ESCAPE_CODES.fetch(code)}m"
+    #
+    # Instance methods for effects.
+    #
+    define_method effect do
+      CapeCod.effect(effect, self)
+    end
+
+    #
+    # Singleton methods for effects.
+    #
+    define_singleton_method effect do |obj = ''|
+      string = obj.to_s
+      code = effect_code_for(effect)
+
+      return escape_sequence_for(code) if string.empty?
+
+      apply_escape_sequence(code, string)
+    end
   end
 
-  #
-  # Prepends the given +string+ with the ANSI escape sequence for the given
-  # escape +code+ and append a reset sequence.
-  #
-  def self.apply_escape_sequence(code, string)
-    sequence = escape_sequence_for(code)
+  class << self
+    def foreground(color, target)
+      apply_escape_sequence(color_code_for(color, :foreground), target)
+    end
 
-    return sequence unless string
+    def background(color, target)
+      apply_escape_sequence(color_code_for(color, :background), target)
+    end
 
-    sequence << string << escape_sequence_for(:reset)
+    def effect(effect, target)
+      apply_escape_sequence(effect_code_for(effect), target)
+    end
+
+    alias_method :color, :foreground
+    alias_method :fg,    :foreground
+    alias_method :bg,    :background
+
+    protected
+
+    def color_code_for(color, ground)
+      COLORS.fetch(color) + (ground == :foreground ? 30 : 40)
+    end
+
+    def effect_code_for(effect)
+      EFFECTS.fetch(effect)
+    end
+
+    private
+
+    #
+    # Returns the ANSI escape sequence for a given escape +code+.
+    #
+    def escape_sequence_for(code)
+      "\e[#{code}m"
+    end
+
+    #
+    # Prepends the given +string+ with the ANSI escape sequence for the given
+    # escape +code+ and append a reset sequence.
+    #
+    def apply_escape_sequence(code, string)
+      sequence = escape_sequence_for(code)
+
+      return sequence unless string
+
+      sequence << string << escape_sequence_for(effect_code_for(:reset))
+    end
   end
 end
--- spec/cape-cod_spec.rb
+++ spec/cape-cod_spec.rb
@@ -11,25 +11,21 @@ describe CapeCod do
     expect(CapeCod.reset).to eq("\e[0m")
   end
 
-  it 'has methods for all defined escape codes' do
-    methods = CapeCod.instance_methods
-
-    CapeCod::ESCAPE_CODES.each do |method, _|
-      expect(methods).to include(method)
-    end
-  end
-
-  context 'when using singleton method' do
+  context 'when using singleton methods' do
     context 'no params given' do
       it 'returns the escape sequence' do
-        expect(CapeCod.red).to eq("\e[31m")
+        expect(CapeCod.red).to    eq("\e[31m")
+        expect(CapeCod.on_red).to eq("\e[41m")
+        expect(CapeCod.bold).to   eq("\e[1m")
       end
     end
 
     context 'when object param given' do
       it 'prepends the escape sequence and append a reset' do
         obj = ['foo', 10, :bar]
-        expect(CapeCod.red(obj)).to eq("\e[31m#{obj.to_s}\e[0m")
+        expect(CapeCod.red(obj)).to    eq("\e[31m#{obj.to_s}\e[0m")
+        expect(CapeCod.on_red(obj)).to eq("\e[41m#{obj.to_s}\e[0m")
+        expect(CapeCod.bold(obj)).to   eq("\e[1m#{obj.to_s}\e[0m")
       end
     end
   end
@@ -42,7 +38,15 @@ describe CapeCod do
     it 'returns a new string with the proper escape codes applied' do
 
       expect(string.red).to_not eql(string)
-      expect(string.red).to eql("\e[31mfoo bar baz\e[0m")
+
+      expect(string.red).to    eql("\e[31mfoo bar baz\e[0m")
+      expect(string.on_red).to eql("\e[41mfoo bar baz\e[0m")
+      expect(string.bold).to   eql("\e[1mfoo bar baz\e[0m")
+
+      expect(string.red.on_yellow).to \
+                                    eql("\e[43m\e[31mfoo bar baz\e[0m\e[0m")
+      expect(string.red.on_yellow.bold).to \
+                          eql("\e[1m\e[43m\e[31mfoo bar baz\e[0m\e[0m\e[0m")
     end
   end
 end
