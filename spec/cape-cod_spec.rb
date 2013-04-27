9ae05aac91eb98935e45ac9ad09da4b164a3d820
Fuad Saud <fuadksd@gmail.com>
2013-04-27 00:40:48 -0300

    Add some tests

---
+++ spec/cape-cod_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe CapeCod do
+  it 'should have a version' do
+    expect(CapeCod::VERSION).to be_a(String)
+  end
+
+  it 'should generate proper escape sequences' do
+    expect(CapeCod.escape_sequence_for(:reset)).to eq("\e[0m")
+  end
+
+  it 'should have methods for all defined escape codes' do
+    methods = CapeCod.instance_methods
+
+    CapeCod::ESCAPE_CODES.each do |method, _|
+      expect(methods).to include(method)
+    end
+  end
+end
